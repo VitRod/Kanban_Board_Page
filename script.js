83a3f4a7ce745ea6ba63f12970a5306fdf5067c6
Vitaliy Rodyna <ukrdao@gmail.com>
2022-10-28 20:48:24 +0300

    // Hide Item Input Box function hideInputBox(column) {   addBtns[column].style.visibility = 'visible';   saveItemBtns[column].style.display = 'none';   addItemContainers[column].style.display = 'none';   addToColumn(column); }

--- script.js
+++ script.js
@@ -140,7 +140,13 @@ function showInputBox(column) {
     addItemContainers[column].style.display = 'flex';
   }
 
-
+// Hide Item Input Box
+function hideInputBox(column) {
+    addBtns[column].style.visibility = 'visible';
+    saveItemBtns[column].style.display = 'none';
+    addItemContainers[column].style.display = 'none';
+    addToColumn(column);
+  }
 
 
 
